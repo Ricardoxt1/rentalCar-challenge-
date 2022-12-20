b154c9d8cd17bfbc52fe11ac74fa6705e9fa7ee9
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2022-12-20 20:39:03 -0300

    alterações diversas

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 ////////////////////////////////////////////////////////////////////////////////////
 
