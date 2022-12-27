98e3965bc3d29aaa6ce94f668f989bbf86c540cc
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2022-12-26 21:18:58 -0300

    att, arquivos complementares

--- mysql.js
+++ mysql.js
@@ -1,11 +1,11 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
 var pool = mysql.createPool({
-    "user": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
 
 });
 
